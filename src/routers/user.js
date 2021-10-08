c9d808252dca44088f3046e22d6a5e678dbe0805
AnuragB <anurag.bh27@gmail.com>
2021-10-08 19:21:00 +0530

    Changed user router

--- src/routers/user.js
+++ src/routers/user.js
@@ -55,7 +55,7 @@ router.post('/users/logoutAll', auth, async (req,res) => {
     }
 })
 
-router.get('users/me', auth, async (req,res) => {
+router.get('/users/me', auth, async (req,res) => {
     res.send(req.user)
 })
 
