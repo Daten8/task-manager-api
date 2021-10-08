8d431f2aba3e480ea88aa9127ab0e3f28682cfa2
AnuragB <anurag.bh27@gmail.com>
2021-10-08 16:30:33 +0530

    Changed require in task router

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,4 +1,4 @@
-const express = require('Express')
+const express = require('express')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
